f47bf4c67ab2c4173a9290cf02bd68372f4fd9ca
Yash <yashp241195@gmail.com>
2020-07-10 04:00:58 +0530

    Update stful.sh

--- React/stful.sh
+++ React/stful.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
+# this is the command
+# ./stful ComponentName withCSS
+
 componentName=$1
-if [ "$2" == "withcss" ]; then withCSS=true; else withCSS=false; fi
+hasCSS=$2
+
+importcss=""
+
+
+if [ "$hasCSS" == "withcss" ]; then withCSS=true; else withCSS=false; fi
 
 if [ -d "../src/components/$componentName" ]; then
-  echo " $1 component already exists"
+  echo " $componentName component already exists"
   exit 1
 fi
 
 mkdir ../src/components/$componentName
 touch ../src/components/$componentName/$componentName.js
-
-importcss=""
 
 if [ "$withCSS" == true ]; then
   touch ../src/components/$componentName/$componentName.css
@@ -20,19 +26,18 @@ fi
 
 echo "component folder created .."
 
-
 cat << EOF >> ../src/components/$componentName/$componentName.js
 import React, {Component} from 'react';
 $importcss
 
 class $componentName extends Component{
-  
+
   /*
     $componentName is defined as a stateful component
   */
 
   render(){
-    
+
     return(
         <div>$componentName</div>
     );
